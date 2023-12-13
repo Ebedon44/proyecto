7b61801f36c0b19703bfbdfb90cc08c9487fc05e
Ebedon44 <86026837+Ebedon44@users.noreply.github.com>
2023-12-12 20:29:38 -0500

    Mapping de Entities

---
+++ src/main/java/org/proyecto/entity/Persona.java
@@ -0,0 +1,60 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package org.proyecto.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ *
+ * @author Erick Bedón
+ */
+@Entity
+@Table(name = "persona")
+public class Persona {
+
+    @Id
+    private Integer id;
+    private String identificacion;
+    private String nombres;
+    private String apellidos;
+
+    public Persona() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getIdentificacion() {
+        return identificacion;
+    }
+
+    public void setIdentificacion(String identificacion) {
+        this.identificacion = identificacion;
+    }
+
+    public String getNombres() {
+        return nombres;
+    }
+
+    public void setNombres(String nombres) {
+        this.nombres = nombres;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+}
